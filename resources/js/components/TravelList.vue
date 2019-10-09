472ac79f40843cc17bea68b49ac5fa3cbbd354aa
Elias De Bock <47783390+Eliasdb@users.noreply.github.com>
2019-10-09 15:52:10 +0200

    stuff

--- resources/js/components/TravelList.vue
+++ resources/js/components/TravelList.vue
@@ -1,126 +1,147 @@
 <template>
-    <div>
+    <div class="container center bg-white shadow">
+        <h2>Travel calculator</h2>
+        <div class="row justify-content-start">
+            <div class="col-4" >
+                <form @submit.prevent="addItem">
+                    <label for="date">Date</label>
+                    <input type="text" placeholder="Date" id="date" class="form-control" v-model="date" required>
+                    <label for="destination">Destination</label>
+                    <input type="text" placeholder="Travel destination" id="destination" class="form-control" v-model="destination" required>
+                    <input type="submit" value="Add" class="btn btn-dark">
+                </form>
+                <form @submit.prevent="saveLocation">
+                    <label for="location">Current location</label>
+                    <input type="text" placeholder="Current location" id="location" class="form-control" v-model="location" required>
+                    <input type="submit" value="Save" class="btn btn-dark">
+                </form>
+            </div>
 
-        <form @submit.prevent="addItem" style="margin-left: 5px">
-            <br>
-            <input type="text" placeholder="Date" style="width: 200px" v-model="date">
-            <br><br>
-            <input type="text" placeholder="Travel destination" style="width: 200px" v-model="destination">
-            <br><br>
-            <input type="submit" value="Add" class="btn btn-primary" style="width:200px">
-            <br><br>
-        </form>
-        <hr>
+            <div class="col-4">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Destination</th>
+                            <th colspan="2">Distance(km)</th>
+                        </tr>
+                        </thead>
 
-        <form @submit.prevent="addLocation" style="margin-left: 5px">
-            <br>
-            <input type="text" placeholder="Current location" style="width: 200px" v-model="location">
-            <br><br>
-            <input type="submit" value="Save" class="btn btn-primary" style="width: 200px">
-            <br><br>
-        </form>
-        <hr>
-
-
-        <div>
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Destination</th>
-                    <th colspan="2">Distance</th>
-
-                </tr>
-                </thead>
-
-                <tbody>
-                <tr v-for="x in items">
-                    <td v-text="x.date"></td>
-                    <td v-text="x.destination"></td>
-                    <td></td>
-                    <td>
-                        <button @click="deleteItem()" style="border: none; background: none"><i class="fas fa-calendar-times"></i></button>
-                        <button @click="showDistance()" style="border: none; background: none"><i class="fas fa-calculator"></i></button>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+                        <tbody>
+                        <tr v-for="x in items">
+                            <td v-text="x.date"></td>
+                            <td v-text="x.destination"></td>
+                            <td v-text="x.distance"></td>
+                            <td>
+                                <button @click="deleteItem()"><i class="fas fa-trash"></i></button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+            </div>
         </div>
-
     </div>
 </template>
 
 <script>
-
-
-
     export default {
         data() {
+            // If the local storage of the array items isn't an array anymore for some reason, make it one again.
+            // Why? To make sure it always saves in an array.
             let items = JSON.parse(localStorage.getItem('travel-items') || '[]')
 
             if(!Array.isArray(items)){
                 items = []
             }
-            let locations = JSON.parse(localStorage.getItem('current-location') || '[]')
+            // If the location isn't a string, make it into one.
+            // Why? When someone inputs not a string, it's going to make it into one, so it doesn't break.
+            let location = JSON.parse(localStorage.getItem('current-location') || '')
 
-            if(!Array.isArray(items)){
-                locations = []
+            if(typeof location !== 'string'){
+                location = ''
             }
 
             return {
                 date: '',
                 destination: '',
                 items, // = items: items
-                location: '',
-                locations,
-                distance: []
+                location,
+                color: {color: '#f8f9fa'}
             };
         },
         methods: {
-            addItem() {
+            // Returns a promise(.get), once that is resolved, .then execute the code.
+            // Why? We want the data first.
+            async calculateDistance(from, to) {
+                return axios.get(`https://nl.afstand.org/route.json?stops=${from}|${to}`)
+                    .then(response => response.data.distance)
+            },
+
+            // Wait until the distance has been calculated before executing other code.
+            // Why? Because you have to wait for the data to be calculated.
+            async addItem() {
                 if (this.items.push({
                     date: this.date,
                     destination: this.destination,
+                    distance: await this.calculateDistance(this.location, this.destination)
                 })) {
                     this.date = '';
                     this.destination = ''
                 }
             },
-            deleteItem() {
+            deleteItem(item) {
                 this.items.splice(
-                    this.items.indexOf(), 1
+                    this.items.indexOf(item), 1
                 );
             },
-            addLocation() {
-                if (this.locations.push({
-                    location: this.location
-                })) {
-                    this.location = ''
-                }
-            },
-            showDistance() {
+            // Store location input in local storage and go through each element of the items array,
+            // waiting until the calculation has ended.
+            // Why? We need the data from the calculation first.
+            saveLocation() {
+                localStorage.setItem('current-location', JSON.stringify(this.location));
 
+                this.items
+                    .forEach(async item => {
+                        item.distance = await this.calculateDistance(this.location, item.destination)
+                    })
             }
         },
         watch: {
             items() {
                 localStorage.setItem('travel-items', JSON.stringify(this.items));
             },
-            locations() {
-                localStorage.setItem('current-location', JSON.stringify(this.locations));
-            }
         },
-
-        created() {
-            axios
-                .get('https://nl.afstand.org/route.json?stops=x|y')
-                .then(response => {
-                    this.distance = response.data;
-                })
-        }
     }
 </script>
 
 
 <style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Volkhov&display=swap');
+
+    .form-control, .btn {
+        width: 200px;
+        margin: 10px;
+    }
+    label {
+        font-weight: bold;
+        font-family: 'Volkhov', serif;
+
+    }
+    h2 {
+        text-align: center;
+        font-family: 'Volkhov', serif;
+        margin-top: 12px;
+        font-weight: bold;
+        padding: 5px;
+
+    }
+    .container {
+        margin-top: 240px;
+    }
+
+    button {
+        border: none;
+        background: none;
+    }
+
 </style>
